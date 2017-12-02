4701f2352ae32fc8750a8e8e4013ebeb8610b5c0
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2017-12-02 22:18:47 +0100

    fastify close hook

--- index.js
+++ index.js
@@ -5,29 +5,35 @@ const MongoInMemory = require('mongo-in-memory')
 const mongodb = require('mongodb')
 
 const fastifyMongoMemory = (fastify, { port, dbname }, next) => {
-  try {
-    const instance = new MongoInMemory(8000 || port)
+  const server = new MongoInMemory(port || 8000)
 
-    instance.start(error => {
+  server.start(error => {
+    if (error) {
+      next(new Error(error))
+    } else {
+      const uri = server.getMongouri(dbname || 'test')
+
+      mongodb.connect(uri, (error, db) => {
+        if (error) {
+          next(new Error(error))
+        } else {
+          fastify.decorate('mongo', db)
+          next()
+        }
+      })
+    }
+  })
+
+  fastify.addHook('onClose', (instance, done) => {
+    server.stop(error => {
       if (error) {
-        next(new Error(error))
+        done(new Error(error))
       } else {
-        const uri = instance.getMongouri(dbname)
-
-        mongodb.connect(uri, (error, db) => {
-          if (!error) {
-            fastify.decorate('mongo', db)
-            fastify.decorate('mongoInstance', instance)
-            next()
-          } else {
-            next(new Error(error))
-          }
-        })
+        fastify.mongo.close()
+        done()
       }
     })
-  } catch (err) {
-    next(new Error(err))
-  }
+  })
 }
 
 module.exports = fastifyPlugin(fastifyMongoMemory, '>=0.30.0')
--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const fastify = require('fastify')()
 const tap = require('tap')
 const fastifyMongoMemory = require('./index')
 
-tap.test('fastify.mongo and fastify.mongoInstance should exist', test => {
-  test.plan(4)
+tap.test('fastify.mongo should exist', test => {
+  test.plan(2)
 
   fastify.register(fastifyMongoMemory, {
     port: 8000,
@@ -15,14 +15,7 @@ tap.test('fastify.mongo and fastify.mongoInstance should exist', test => {
   fastify.ready(err => {
     test.error(err)
     test.ok(fastify.mongo)
-    test.ok(fastify.mongoInstance)
 
-    fastify.mongo.close()
-    fastify.mongoInstance.stop(error => {
-      test.error(error)
-      test.end()
-    })
-
-    fastify.close()
+    fastify.close(() => test.end())
   })
 })
