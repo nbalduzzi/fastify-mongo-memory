b02cc3ede9cf47a82c938a4ec81994fc44749d2f
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2017-12-02 13:28:46 +0100

    new version

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const fastify = require('fastify')()
 const tap = require('tap')
 const fastifyMongoMemory = require('./index')
 
-tap.test('fastify.mongoMemory should exist', test => {
-  test.plan(3)
+tap.test('fastify.mongo and fastify.mongoInstance should exist', test => {
+  test.plan(4)
 
   fastify.register(fastifyMongoMemory, {
     port: 8000,
@@ -15,6 +15,7 @@ tap.test('fastify.mongoMemory should exist', test => {
   fastify.ready(err => {
     test.error(err)
     test.ok(fastify.mongo)
+    test.ok(fastify.mongoInstance)
 
     fastify.mongo.close()
     fastify.mongoInstance.stop(error => {
